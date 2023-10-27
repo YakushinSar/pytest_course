081b5e14eb07081f0233c10e531ea99799d54541
YakushinSar <saratov482043162@gmail.com>
2023-10-27 14:46:42 +0400

    add all files

---
+++ hw3/herokuapp/locators.py
@@ -0,0 +1,15 @@
+# ADD/REMOVE ELEMENTS
+BUTTON_ADD_ELEMENT = '//button[contains(text(), "Add Element")]'
+BUTTON_DELETE = '//button[@class="added-manually"]'
+BUTTON_DELETE_ANY = '//button[@class="added-manually"]'
+
+# BASIC AUTH PAGE
+TITLE_BASIC_AUTH_PAGE = '//h3'
+TEXT_SUCCESSFUL_AUTH = '//p[contains(text(), "Congratulations!")]'
+
+# BROKEN IMAGES PAGE
+IMG_ANY = '//div[@class="example"]/img'
+
+# CHECKBOXES PAGE
+TICKED_CHECKBOX_ANY = '//input[@type="checkbox" and @checked]'
+NOT_TICKED_CHECKBOX_ANY = '//input[@type="checkbox" and not(@checked)]'
